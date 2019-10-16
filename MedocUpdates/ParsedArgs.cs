873e1efbe2e283ca2dbddf5ce162f3d279c93b54
UAVXP <uavxp29@gmail.com>
2019-10-16 16:31:26 +0300

    ParsedArgs dummy

---
+++ MedocUpdates/ParsedArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedocUpdates
+{
+	public static class ParsedArgs
+	{
+		public class ArgsPair
+		{
+			string token;
+			string arg;
+		}
+
+		public static List<ArgsPair> args = new List<ArgsPair>();
+	}
+}
