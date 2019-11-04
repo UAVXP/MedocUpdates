7ea86b3dd2fad3d2ef7ca5ed655dd36a0899c078
UAVXP <uavxp29@gmail.com>
2019-11-04 12:43:14 +0200

    TelegramChatsStorage logging

--- MedocUpdates/TelegramChatsStorage.cs
+++ MedocUpdates/TelegramChatsStorage.cs
@@ -30,7 +30,10 @@ namespace MedocUpdates
 
 			// Checking again
 			if (!File.Exists(m_storageFilePath))
+			{
+				Log.Write(LogLevel.NORMAL, "TelegramChatsStorage: Save(): Cannot create chatID storage file");
 				return false;
+			}
 
 			string chatIDsFileStr = "";
 			foreach(long chatID in m_TelegramChatIDs)
@@ -39,8 +42,11 @@ namespace MedocUpdates
 			}
 
 			File.WriteAllText(m_storageFilePath, chatIDsFileStr);
-			if (!File.Exists(m_storageFilePath))
+			if (!File.Exists(m_storageFilePath)) // TODO: Check for file size instead of availability
+			{
+				Log.Write(LogLevel.NORMAL, "TelegramChatsStorage: Save(): Storage file is gone in the middle of writing");
 				return false; // And here we're going to fail
+			}
 
 			return true;
 		}
@@ -48,19 +54,31 @@ namespace MedocUpdates
 		public static bool Restore()
 		{
 			if (!File.Exists(m_storageFilePath))
+			{
+				Log.Write(LogLevel.NORMAL, "TelegramChatsStorage: Restore(): Cannot restore - storage file doesn't exist");
 				return false;
+			}
 
 			string chatIDsStr = File.ReadAllText(m_storageFilePath);
 			if(chatIDsStr.Trim().Length <= 0)
+			{
+				Log.Write(LogLevel.NORMAL, "TelegramChatsStorage: Restore(): Cannot restore - storage file is empty. Using the empty list instead");
 				return false;
+			}
 
 			string[] userIDsStr = chatIDsStr.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
 			if(userIDsStr.Length <= 0)
+			{
+				Log.Write(LogLevel.NORMAL, "TelegramChatsStorage: Restore(): Cannot restore - parsing error");
 				return false;
+			}
 
 			long[] userIDs = Array.ConvertAll(userIDsStr, long.Parse);
 			if(userIDs.Length <= 0)
+			{
+				Log.Write(LogLevel.NORMAL, "TelegramChatsStorage: Restore(): Cannot restore - convertation error");
 				return false;
+			}
 
 			m_TelegramChatIDs.AddRange(userIDs);
 			return true;
